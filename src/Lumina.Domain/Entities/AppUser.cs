fc1c8cf484a272104b3585e0da3986e68c7c04e5
scoriat1 <39129248+scoriat1@users.noreply.github.com>
2026-03-05 11:49:58 -0500

    Fixed guid ids.

--- src/Lumina.Domain/Entities/AppUser.cs
+++ src/Lumina.Domain/Entities/AppUser.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Lumina.Domain.Entities;
 
-public class AppUser : IdentityUser<string>
+public class AppUser : IdentityUser
 {
     public string DisplayName { get; set; } = string.Empty;
     public ICollection<Provider> Providers { get; set; } = new List<Provider>();
